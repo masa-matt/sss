0c0a89f55b2c4b781b13d8b945582db108051f2e
masa-matt <masatoshi.ito@hashport.io>
2025-03-28 16:40:01 +0100

    fix combine

--- combine.js
+++ combine.js
@@ -1,12 +1,20 @@
 import secrets from "secrets.js-grempe";
+import assert from "assert";
 import dotenv from "dotenv";
 dotenv.config();
 
-const share1 = process.env.SHARE1 || "";
-const share2 = process.env.SHARE2 || "";
-const share3 = process.env.SHARE3 || "";
+const shares = process.env.SHARES || 3;
+const threshold = process.env.THRESHOLD || 2;
 
-const sharesToRecover = [share1, share2, share3].filter((s) => s !== "");
+const sharesToRecover = [...Array(shares)]
+  .map((_, i) => i + 1)
+  .map((i) => process.env[`SHARE${i}`])
+  .filter((s) => s !== "");
+
+assert(
+  sharesToRecover.length >= threshold,
+  "env SHARE<#index> is required more than threshold",
+);
 
 const recoveredHex = secrets.combine(sharesToRecover);
 const recoveredSecret = secrets.hex2str(recoveredHex);
